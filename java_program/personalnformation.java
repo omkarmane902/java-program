dbef88e02b3c50a3676fa18a95ef03a47aa909d1
onkar Mane <157266293+omkarmane902@users.noreply.github.com>
2025-03-20 23:29:58 +0530

    Add files via upload

---
+++ java_program/personalnformation.java
@@ -0,0 +1,13 @@
+class personalnformation
+{
+	public static void main(String[]args)
+	{
+		System.out.println("Name:Omkar Dhanaji mane");
+		System.out.println("Address:yadrav");
+		System.out.println("Age:22year");
+		System.out.println("Collage Name: nanasaheb mahadik collage of engineering,peth");
+		System.out.println("branch:computer science engineering");
+		System.out.println("class:4year");
+		System.out.println("current location:pune");
+	}
+}
