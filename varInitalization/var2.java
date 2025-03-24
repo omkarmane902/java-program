9e923e54b71902f24f792af3e7f6a9a5f597ecd3
onkar Mane <157266293+omkarmane902@users.noreply.github.com>
2025-03-24 22:17:10 +0530

    Add files via upload

---
+++ varInitalization/var2.java
@@ -0,0 +1,25 @@
+// varibal we declare and initialization statement:- 
+
+//here we declare the variable and store value simultaneously so we say
+// syntax:- 
+//           datatypeVariableName = value/expression
+
+class var2
+{
+public static void main(String[]args)
+{
+	String name = "omkar mane";
+	byte age =22;
+	float salary = 50000000.00f;
+	char grade = 'A';
+	long phoneNumber=8625906485L;
+	boolean greatMan = true;
+	
+	System.out.println(name);
+	System.out.println(age);
+	System.out.println(salary);
+	System.out.println(grade);
+	System.out.println(phoneNumber);
+	System.out.println(greatMan);
+}
+}
---
+++ varInitalization/varInitalization.java
@@ -0,0 +1,27 @@
+// Variable Initalization :-
+//                            It is statement use to assign the value inside the varible
+
+// Syntax:-  variableName=value/expression
+
+class varInitalization
+{
+public static void main(String[]args)
+{
+	String name;
+	byte score;
+	float hight;
+	char grade;
+	 
+	name="omkar";
+	score=106;
+	hight=5.7f;
+	grade='A';
+	
+	System.out.println(name);
+	System.out.println(score);
+	System.out.println(hight);
+	System.out.println(grade);
+	
+}
+}
+
