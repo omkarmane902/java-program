567633be0912bcc43c5257d847712e6cc2a432fb
onkar Mane <157266293+omkarmane902@users.noreply.github.com>
2025-04-08 00:11:53 +0530

    Add files via upload

---
+++ Operators/Condition operator/condition1.java
@@ -0,0 +1,13 @@
+//condition of positive and nigitive finding
+
+class condition1
+{
+	public static void main(String[]args)
+	{
+		
+		int num = 10;
+		
+		String  ans = num>0?"positive":"Negitive";
+		System.out.println(ans);
+	}
+}
---
+++ Operators/Condition operator/condition2.java
@@ -0,0 +1,13 @@
+//condition of even and odd finding
+
+
+class condition2
+{
+	public static void main (String[]args)
+	{
+		int num = 37;
+		
+		String ans = num%2==0?"Even":"Odd";
+		System.out.println(ans);
+	}
+}
