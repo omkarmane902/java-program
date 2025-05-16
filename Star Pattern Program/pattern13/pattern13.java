ca5130655122a22376766f65764fff2bafd0427f
onkar Mane <157266293+omkarmane902@users.noreply.github.com>
2025-05-16 16:33:34 +0530

    Add files via upload

---
+++ Star Pattern Program/pattern13/pattern13.java
@@ -0,0 +1,21 @@
+class pattern13
+{
+	public static void main (String [] args)
+	{
+		int n = 10;
+		
+		for (int i = 0; i<n; i++)
+		{
+			for(int space = 0 ; space<n-i-1 ; space++)
+			{
+				System.out.print(" "+" ");
+			}
+			   for(int star = 0; star<=2*i; star++)
+			    {
+			       System.out.print("*"+" ");	
+			    }
+				System.out.println();
+		}
+       
+	}
+}
