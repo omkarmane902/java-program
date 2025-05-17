3986ee6b77a980510a52be69a7a493806cd1bc4c
onkar Mane <157266293+omkarmane902@users.noreply.github.com>
2025-05-17 22:52:33 +0530

    Add files via upload

---
+++ Star Pattern Program/pattern22/pattern22.java
@@ -0,0 +1,17 @@
+class pattern22
+{
+	public static void main (String []args)
+	{
+		int n = 5 ;
+		char ch = 'a';
+		
+		for(int i=1; i<n; i++)
+		{
+			for(int j=1; j<n; j++)
+			{
+				System.out.print((ch++)+" ");
+			}
+			System.out.println();
+		}
+	}
+}
