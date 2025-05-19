80efae03c39e5432d7a1e66b1a2cb41ec2bc83a4
onkar Mane <157266293+omkarmane902@users.noreply.github.com>
2025-05-19 23:00:56 +0530

    Add files via upload

---
+++ Star Pattern Program/pattern26/pattern26.java
@@ -0,0 +1,23 @@
+class pattern26
+{
+	public static void main (String[]args)
+	{
+		int n = 8;
+		
+		for(int i = 0; i<n; i++)
+		{
+			for(int j = 0; j<n; j++)
+			{
+				if(i==j || i==0 || i+j==n-1 || i==n-1)
+				{
+				   System.out.print("*"+" ");	
+				}
+				else
+				{
+					System.out.print(" "+" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
