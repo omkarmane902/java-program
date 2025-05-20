2119500f311bfaa0b3ae1ecc26b5ee21c942c2c6
onkar Mane <157266293+omkarmane902@users.noreply.github.com>
2025-05-20 21:24:58 +0530

    Add files via upload

---
+++ Methods program/Methods program _1/Methodsprogram1.java
@@ -0,0 +1,23 @@
+class Methodsprogram1
+{
+	public static void main(String[] args)
+	{
+	 chi();
+     om();
+     abhi();	 
+	}
+	public static void om()
+	{
+		System.out.println("hi");
+	}
+	
+	public static void chi()
+	{
+		System.out.println("hi intership");
+	}
+	
+	public static void abhi()
+	{
+		System.out.println("rata mar");
+	}
+}
