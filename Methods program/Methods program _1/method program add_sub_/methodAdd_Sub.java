af48b60f43da8895cd55555d5af58e2c78bb5b46
onkar Mane <157266293+omkarmane902@users.noreply.github.com>
2025-05-21 23:50:33 +0530

    Add files via upload

---
+++ Methods program/Methods program _1/method program add_sub_/methodAdd_Sub.java
@@ -0,0 +1,24 @@
+class methodAdd_Sub
+{
+	public static void main(String[] args)
+	{
+	   add(100,20);
+       sub(12,10);
+       add1(40,10);	   
+	}
+	public static void add(int a,int b)
+	{
+		 int ans = a + b;
+		 System.out.println(ans);
+	}
+	public static void sub(int a,int b)
+	{
+		int ans = a = b;
+		 System.out.println(ans);
+	}
+	public static void add1(int a,int b)
+	{
+		int ans = a + b;
+		 System.out.println(ans);
+	}
+}
