d002f15b9ddf0885b28a5e4aec3809f1e1540699
onkar Mane <157266293+omkarmane902@users.noreply.github.com>
2025-05-28 17:24:35 +0530

    Add files via upload

---
+++ Methods program/Recurssion  Method    fibonacci number/RecurssionMethodFibo.java
@@ -0,0 +1,19 @@
+class RecurssionMethodFibo
+{
+	public static void main (String [] args)
+	{
+		fibo(10,0,1);
+	}
+	public static void fibo(int count , int n1, int n2 ) 
+	{
+		if(count >0) 
+		{
+		System.out.println(n1); 
+		
+		fibo (count-1, n2 , n1 + n2);  
+	    }
+		
+	}//
+	
+	
+}
