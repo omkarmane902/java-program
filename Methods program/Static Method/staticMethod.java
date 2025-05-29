92faf019dce5bf963d888a32ee212de4c72dd291
onkar Mane <157266293+omkarmane902@users.noreply.github.com>
2025-05-29 18:09:02 +0530

    Add files via upload

---
+++ Methods program/Static Method/staticMethod.java
@@ -0,0 +1,9 @@
+// different method access in on class but accessed program are present in the one folder 
+class staticMethod
+{
+	public static void main(String[]args)
+	{
+	   primeNumber.prime(43);
+       System.out.println(palindromicPrime.palindromicno(131));   
+	}
+}
