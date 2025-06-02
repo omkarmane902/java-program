ad66ca03fd8754de4eebe3ab09d5eb71c4c898af
onkar Mane <157266293+omkarmane902@users.noreply.github.com>
2025-06-02 21:53:56 +0530

    Add files via upload

---
+++ Methods program/public class/abc.java
@@ -0,0 +1,12 @@
+public class abc 
+{
+	public static void main (String[]args)
+	{
+		System.out.println("from abc");
+		cba();
+	}
+	public static void cba()
+	{
+	   System.out.println("from cba");	
+	}
+}
