cafd65e908d263c752f70a6ad1f76774d45ee486
onkar Mane <157266293+omkarmane902@users.noreply.github.com>
2025-06-06 09:11:03 +0530

    Add files via upload

---
+++ OOP/example 3/mobile.java
@@ -0,0 +1,25 @@
+class mobile
+{
+	String brand ;
+	String Color;
+	double DisplaySize;
+	int Storage;
+	int Camera;
+	int WarrantyYerar;
+	int RAM;
+	int ROM;
+	double price;
+	
+	public void displayMobile()
+	{
+	    System.out.println("brand :"+ brand);
+		System.out.println("price:"+ price);
+		System.out.println("Color :"+ Color);
+		System.out.println("Display Size :" + DisplaySize);
+		System.out.println("Storage :"+ Storage);
+		System.out.println("Warranty Yerar :"+ WarrantyYerar);
+		System.out.println("RAM:"+ RAM);
+		System.out.println("ROM:"+ ROM);
+	}
+
+}
