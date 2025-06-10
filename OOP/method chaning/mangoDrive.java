e94ac5856c32973ac168013a42a70eac31bbed58
onkar Mane <157266293+omkarmane902@users.noreply.github.com>
2025-06-10 22:36:28 +0530

    Add files via upload

---
+++ OOP/method chaning/mangoDrive.java
@@ -0,0 +1,15 @@
+class mangoDrive
+{
+	public static void main(String[] args)
+	{
+		mango m1 = new mango();
+		
+		m1.name = "hapus";
+		m1.price = 1200.0;
+		m1.origin = "Ratnagiri"; 
+		m1.mangoname(). mangoprice().mangoorigin();
+	
+		
+		
+	}
+}
