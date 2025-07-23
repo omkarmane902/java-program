148efcde3834c49542a6487bb4a9dff490868dc4
onkar Mane <157266293+omkarmane902@users.noreply.github.com>
2025-07-23 19:41:51 +0530

    Add files via upload

---
+++ Exception_handling/Throws_Exception_handling/exp1.java
@@ -0,0 +1,15 @@
+package Throws_Exception_handling;
+
+
+public class exp1 {
+    public static void main(String[] args) throws InterruptedException {
+    	
+    for (int i = 0; i < 10; i++) {
+		
+		System.out.println(" hi omkar ");
+		
+		Thread.sleep(2000);
+	}
+	
+}
+}
