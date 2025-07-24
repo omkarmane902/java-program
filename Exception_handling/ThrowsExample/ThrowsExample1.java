525185020763a5c8cc847f025d59f297ae8d406d
onkar Mane <157266293+omkarmane902@users.noreply.github.com>
2025-07-24 19:04:09 +0530

    Add files via upload

---
+++ Exception_handling/ThrowsExample/ThrowsExample1.java
@@ -0,0 +1,12 @@
+package ThrowsExample;
+
+
+
+public class ThrowsExample1 {
+public static void main(String[] args) throws  InterruptedException {
+	 System.out.print("hi");
+	 
+	 Thread.sleep(3000);
+	 System.out.print(" omkar mane");
+}
+}
