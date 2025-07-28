407491e51da71458edac635cfe48d9ed95a29ca8
onkar Mane <157266293+omkarmane902@users.noreply.github.com>
2025-07-28 23:11:54 +0530

    Add files via upload

---
+++ access_modifiers/Default/exp1.java
@@ -0,0 +1,24 @@
+package Default;
+
+public class exp1 {
+
+	int a = 10;
+	
+	
+	
+	 exp1() {
+		// TODO Auto-generated constructor stub
+	}
+	 
+	 void test()
+	 {
+		 System.out.println(a);
+		 
+		 System.out.println(" hi omkar ");
+	 }
+	 
+	 
+	
+	 
+	
+}
---
+++ access_modifiers/Default/exp2.java
@@ -0,0 +1,11 @@
+package Default;
+
+public class exp2  extends exp1{
+ public static void main(String[] args) {
+	 exp1 e1 =  new exp2();
+	 
+	 System.out.println(e1.a);
+	 
+     e1.test();
+}
+}
