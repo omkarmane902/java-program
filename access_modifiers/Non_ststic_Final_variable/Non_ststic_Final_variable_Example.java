574a325e02c3cb47d673e4f86d2291c4a3f7497d
onkar Mane <157266293+omkarmane902@users.noreply.github.com>
2025-07-28 22:36:34 +0530

    Add files via upload

---
+++ access_modifiers/Non_ststic_Final_variable/Non_ststic_Final_variable_Example.java
@@ -0,0 +1,35 @@
+package Non_ststic_Final_variable;
+
+public class Non_ststic_Final_variable_Example {
+
+	
+	final int a = 10 ;
+	
+	final int b ;
+	
+	final char ch = 'b';
+	
+	{
+		b = 20;
+	}
+	
+
+	
+	
+	public Non_ststic_Final_variable_Example()
+	{
+		System.out.println(a);
+		
+		System.out.println(b);
+	}
+	
+	public static void main(String[] args) {
+		
+       // System.out.println(a);   CTE
+		
+	  //System.out.println(b);   CTE
+		
+	}
+	
+	
+}
