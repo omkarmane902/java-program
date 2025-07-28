9c23be88b4180b97be7b18b62fdc7594c2a6e96b
onkar Mane <157266293+omkarmane902@users.noreply.github.com>
2025-07-28 22:12:47 +0530

    Add files via upload

---
+++ access_modifiers/Final/FinalExample.java
@@ -0,0 +1,17 @@
+package Final;
+
+public class FinalExample {
+	
+	public static void main(String[] args) {
+		
+		final int a;
+		
+		a =  10 ;
+		
+	
+		System.out.println(a);
+		
+		//a = 20;  //CTE
+	}
+
+}
