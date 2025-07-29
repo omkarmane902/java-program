acc1e30c60f03d924925e18dfceef8251d0fd7ad
onkar Mane <157266293+omkarmane902@users.noreply.github.com>
2025-07-29 20:28:16 +0530

    Add files via upload

---
+++ access_modifiers/Final_method/final1.java
@@ -0,0 +1,14 @@
+package Final_method;
+
+public class final1 {
+	
+	public final void test()
+	{
+		 System.out.println("you cannot overide me ");
+	}
+	public static final void demo()
+	{
+		System.out.println("I am static & final ");
+	}
+
+}
---
+++ access_modifiers/Final_method/final2.java
@@ -0,0 +1,24 @@
+package Final_method;
+
+public class final2  extends final1{
+ 
+	
+	public void test2()
+	{
+		test();
+		
+		demo();
+	}
+	
+	
+	//public final void test()
+	//{
+		 //System.out.println("you cannot overide me ");
+	//}
+	
+	//public static final void demo()
+	//{
+		//System.out.println("you cannot overide this method");
+	//}
+
+}
