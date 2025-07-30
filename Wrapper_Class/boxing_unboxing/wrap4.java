36adcd80b32edad08493346db749e174d400d356
onkar Mane <157266293+omkarmane902@users.noreply.github.com>
2025-07-30 22:40:54 +0530

    Add files via upload

---
+++ Wrapper_Class/boxing_unboxing/wrap4.java
@@ -0,0 +1,29 @@
+package boxing_unboxing;
+
+public class wrap4 {
+     public static void main(String[] args) {
+    	 
+    	 System.out.println(Integer.MAX_VALUE);
+	     System.out.println(Integer.MIN_VALUE);
+	     
+	     
+	     System.out.println(Byte.MAX_VALUE);
+	     System.out.println(Byte.MIN_VALUE);
+	     
+	     
+	     System.out.println(Long.MAX_VALUE);
+	     System.out.println(Long.MIN_VALUE);
+	     
+	     System.out.println(Float.MAX_VALUE);
+	     System.out.println(Float.MIN_VALUE);
+	     
+	     System.out.println(Double.MAX_VALUE);
+	     System.out.println(Double.MIN_VALUE);
+	     
+	     
+	     System.out.println(Character.MAX_VALUE);
+	     System.out.println(Character.MIN_VALUE);
+	     
+     
+}
+}
