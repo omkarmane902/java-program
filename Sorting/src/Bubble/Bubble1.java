c1fab9fa120fd0cd07a6db27de5ed51a36998e3a
onkar Mane <157266293+omkarmane902@users.noreply.github.com>
2025-08-14 23:22:16 +0530

    Update Bubble1.java

--- Sorting/src/Bubble/Bubble1.java
+++ Sorting/src/Bubble/Bubble1.java
@@ -18,6 +18,9 @@ public class Bubble1 {
 	
  public static void  Bubble11 (int [] arr) {
 	 int j ;
+
+	 	 //time complexity = O(n²).
+	 
 	 for(int i = 0 ; i < arr.length-1; i++ )
 	 {
 		for(j = 0 ; j < arr.length-i-1; j++)
@@ -25,6 +28,8 @@ public class Bubble1 {
 		    
 			if(arr[j] < arr[j+1])
 			{
+
+				//swap
 				int temp = arr[j];
 				
 				arr[j] = arr[j+1];
@@ -40,3 +45,4 @@ public class Bubble1 {
 }
 
 }
+
