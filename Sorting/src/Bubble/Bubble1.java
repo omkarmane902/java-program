f107e072570370224f451d3ab46b778a893ae349
onkar Mane <157266293+omkarmane902@users.noreply.github.com>
2025-08-14 23:15:24 +0530

    Add files via upload

---
+++ Sorting/src/Bubble/Bubble1.java
@@ -0,0 +1,42 @@
+package Bubble;
+
+import java.util.Arrays;
+
+public class Bubble1 {
+	
+	
+	public static void main(String[] args) {
+		
+		int [] arr = {1,30,4,29,45,89};
+		
+		
+		Bubble11(arr);
+		
+	}
+	
+	
+	
+ public static void  Bubble11 (int [] arr) {
+	 int j ;
+	 for(int i = 0 ; i < arr.length-1; i++ )
+	 {
+		for(j = 0 ; j < arr.length-i-1; j++)
+		{
+		    
+			if(arr[j] < arr[j+1])
+			{
+				int temp = arr[j];
+				
+				arr[j] = arr[j+1];
+				
+				arr[j+1] = temp;
+			}
+		}
+	 }
+	 
+	 System.out.println(Arrays.toString(arr));
+	 
+	 
+}
+
+}
