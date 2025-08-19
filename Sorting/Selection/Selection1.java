e8566cbe990852d6822c6f8c471517f8fc04f10f
onkar Mane <157266293+omkarmane902@users.noreply.github.com>
2025-08-19 20:38:15 +0530

    Add files via upload

---
+++ Sorting/Selection/Selection1.java
@@ -0,0 +1,39 @@
+package Selection;
+
+import java.util.Arrays;
+
+public class Selection1 {
+
+	
+	
+	public static void main(String[] args) {
+		
+		
+		int [] arr = {29,10,14,37,13};
+		
+		for (int i = 0; i < arr.length; i++) {
+			
+			
+			int smallest = i;
+			
+			for (int j =i+1; j < arr.length; j++) {
+				
+				if(arr[smallest] > arr[j])
+				{
+					smallest = j;
+				}
+				
+			}
+			
+			int temp = arr[smallest];
+			
+			arr[smallest]= arr[i];
+			
+			arr[i]= temp;
+			
+		}
+		
+		
+		System.out.println(Arrays.toString(arr));
+	}
+}
