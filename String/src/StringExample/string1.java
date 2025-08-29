2ec9e21da422382bed9459c5563d7016f18de9ed
onkar Mane <157266293+omkarmane902@users.noreply.github.com>
2025-08-29 19:03:49 +0530

    Add files via upload

---
+++ String/src/StringExample/string1.java
@@ -0,0 +1,14 @@
+package StringExample;
+
+public class string1 {
+
+	
+	public static void main(String[] args) {
+		
+		
+		String s1 = new String("hi");
+		
+		
+		System.out.println(s1);
+	}
+}
---
+++ String/src/StringExample/string2.java
@@ -0,0 +1,32 @@
+package StringExample;
+
+public class string2 {
+ public static void main(String[] args) {
+	
+	 String s1 = new String("hi");
+	 
+	 System.out.println(s1);
+	 
+	 
+
+	 
+	 String s2 = new String("hi");
+	 
+	 
+	 
+	 
+	 System.out.println(s1==s2);
+	 
+	 System.out.println(s1.equals(s2));
+	 
+	System.out.println(s1.hashCode());
+	
+	System.out.println(s2.hashCode());
+	
+	
+	String s3 = new String("hello");
+			
+	System.out.println(s3);
+;	 
+}
+}
