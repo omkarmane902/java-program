625a4e7c4c0e2f62ba0c6315df6d3e31e43feab5
onkar Mane <157266293+omkarmane902@users.noreply.github.com>
2025-09-03 00:08:55 +0530

    Add files via upload

---
+++ String/src/StringExample/String_convert_into_upercase_useing_inbuilt_function.java
@@ -0,0 +1,23 @@
+package StringExample;
+
+public class String_convert_into_upercase_useing_inbuilt_function {
+
+	
+	
+	public static void main(String[] args) {
+		
+		
+		
+		String s1 = "omkar mane";
+		
+		
+		String result = s1.toUpperCase();
+		
+		
+		
+		
+		System.out.println("lower case : "+s1);
+		
+		System.out.println("Upper case : "+result);
+	}
+}
