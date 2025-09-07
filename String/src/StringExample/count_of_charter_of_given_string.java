99045f683b1e00d806f263bc53bd1e765147eff9
onkar Mane <157266293+omkarmane902@users.noreply.github.com>
2025-09-07 22:26:18 +0530

    Add files via upload

---
+++ String/src/StringExample/count_of_charter_of_given_string.java
@@ -0,0 +1,17 @@
+package StringExample;
+
+public class count_of_charter_of_given_string {
+
+	
+	public static void main(String[] args) {
+		
+		
+		String s1 = " omkar mane is good boy ";
+		
+		
+		s1 = s1.replace(" ", "");
+		
+		
+		System.out.println("count of charter of given string is  : " + s1.length());
+	}
+}
