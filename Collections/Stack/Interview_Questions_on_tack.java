3e1ff45d8ea6617a4485286c942d2da7965c07e5
onkar Mane <157266293+omkarmane902@users.noreply.github.com>
2025-10-15 12:20:16 +0530

    Add files via upload

---
+++ Collections/Stack/Interview_Questions_on_tack.java
@@ -0,0 +1,44 @@
+package Stack;
+
+public class Interview_Questions_on_tack {
+
+	
+	
+	//1) What is Stack and how does it work?
+	
+	    //Stack is a LIFO structure; last inserted element is first removed.
+	
+	
+	
+	//2)What is the difference between Stack and Queue?
+
+			//Stack	                            Queue
+	//LIFO (Last In First Out)	          FIFO (First In First Out)
+	//Uses push()/pop()	                  Uses add()/remove()
+	
+	
+	
+	
+	//3) Is Stack synchronized?
+	
+	    //Yes, java.util.Stack is synchronized (thread-safe).
+	
+	
+	//4)How can you implement Stack without using Stack class?
+	
+	    //By using ArrayList or LinkedList.
+	
+	
+	// 5) When would you use Stack in real projects?
+	
+	//→ For:
+
+		//Undo/Redo operations
+
+		//Browser back/forward
+
+		//Expression evaluation
+
+		//Recursion tracking
+	
+}
