b8ce7c4fd8fc2b8167b4ea437eb68e76b01a2d2e
onkar Mane <157266293+omkarmane902@users.noreply.github.com>
2025-11-04 20:24:42 +0530

    Add files via upload

---
+++ Collections/Vector/Vector2.java
@@ -0,0 +1,32 @@
+
+
+
+                                              // Vector //
+
+
+
+// 1) Vector is like a resizable array in Java.
+
+// 2) It is present in the java.util package.
+
+// 3) It stores data in order (index form) — like 0, 1, 2, 3....
+
+// 4) It can grow automatically when it becomes full.
+
+// 5) Default size = 10 elements.
+
+// 6) When full, its size doubles automatically.
+
+// 7) It allows duplicate values.
+
+// 8) It allows null values.
+
+// 9) It is synchronized — means thread safe (only one thread works at a time).
+
+// 10) It is slower than ArrayList because of synchronization.
+
+// 11) It is an old (legacy) class (from Java 1.0). 
+
+// 12) We can add, remove, update, and access elements easily using methods.
+
+// 13) We can go through elements using for loop, iterator, or enumeration.
